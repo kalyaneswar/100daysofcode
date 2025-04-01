b2b9e1ac044d687321fc87ef25180ec1e8612eaf
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-04-01 13:38:18 +0530

    python

---
+++ Day-18/random_walk.py
@@ -0,0 +1,17 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+
+directions = [90, 180, 270, 0]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
