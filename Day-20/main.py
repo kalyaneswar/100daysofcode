5aba395e9ad949f6810bafef5cc2c6ccc4ae8677
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-05-06 16:43:25 +0530

    python

--- Day-20/main.py
+++ Day-20/main.py
@@ -40,10 +40,14 @@ while game_is_on:
     # Detect collision with tail
     # if head collide with any of the tail or segment
         # Trigger game over
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # for segment in snake.segments:
+    #     if segment == snake.head:
+    #         pass
+    #     elif snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.gameover()
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.gameover()
 
