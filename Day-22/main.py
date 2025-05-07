b8d765b48335ec42827a9d2c45b65f80f9c48efe
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-05-07 17:07:03 +0530

    python create ball and move

--- Day-22/main.py
+++ Day-22/main.py
@@ -2,6 +2,8 @@
 
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,7 +13,7 @@ screen.tracer(0)
 
 l_paddle = Paddle((350, 0))
 r_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 
 screen.listen()
@@ -23,7 +25,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
