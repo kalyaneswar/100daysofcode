17d0fbef22569dc98485df9c476e030729de2123
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-05-08 11:23:03 +0530

    python completed ping game development

--- Day-22/main.py
+++ Day-22/main.py
@@ -3,6 +3,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scorecard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,10 +12,11 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-l_paddle = Paddle((350, 0))
-r_paddle = Paddle((-350, 0))
-ball = Ball()
+r_paddle = Paddle((350, 0))   # Right paddle at +350
+l_paddle = Paddle((-350, 0))  # Left paddle at -350
 
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -25,9 +27,28 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+
+    # Detect Collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Needs to bounce_y
+        ball.bounce_y()
+
+    # Detect collision with R_Paddle
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    # Detect when r paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect when l peddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
