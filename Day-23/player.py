330c1594a07ef76bffb1cd95c9437af198148455
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-05-13 11:16:19 +0530

    python updates score boards

--- Day-23/player.py
+++ Day-23/player.py
@@ -12,11 +12,19 @@ class Player(Turtle):
         self.shape("turtle")
         self.color("green")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
     
     def go_up(self):
         # new_y = self.ycor() + MOVE_DISTANCE
         self.forward(MOVE_DISTANCE)
+    
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
 
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
 
--- Day-23/scoreboard.py
+++ Day-23/scoreboard.py
@@ -1,5 +1,30 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+ALIGNMENT='left'
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(-260,260)
+        self.level = 1
+        self.color("black")
+        self.update_scoreboard()
+        
+    
+    def update_scoreboard(self):
+        # self.clear()
+        self.write(f"Level: {self.level}",  align=ALIGNMENT, font=FONT)
+    
+    def increase_level(self):
+        self.level += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Game Over",  align="center", font=FONT)
 
 
-class Scoreboard:
-    pass
