123e5cbda4ffbfda2c36941d584d613224f946dd
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-05-15 23:06:43 +0530

    python mail merge project to send letters

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,7 +1,25 @@
 #TODO: Create a letter using starting_letter.txt 
 
+PLACEHOLDER = "[name]"
+
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+    print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+        # print(new_letter)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+
+
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
+
 #Save the letters in the folder "ReadyToSend".
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
